b6e658ece523763c2b901b82c8300aa0fc217179
kayes-shawon <shawon.kayes@gmail.com>
2021-11-22 01:13:09 +0600

    Updated minor change

--- proto/server.go
+++ proto/server.go
@@ -6,10 +6,10 @@ type Server struct {
 
 }
 
-//func (s *Server) mustEmbedUnimplementedGreeterServer() {
-//	panic("implement me")
-//}
+func (s *Server) mustEmbedUnimplementedGreeterServer() {
+	panic("implement me")
+}
 
 func (s *Server) SayHello(_ context.Context,in *HelloRequest) (*HelloReply, error) {
-	return &HelloReply{Message: "Hello From the Server! -- " + in.Name}, nil
+	return &HelloReply{Message: "Hello From the Server! -- " + in.GetName()}, nil
 }
